f49bd126a559c4b6c0497f4ac4779b6399532c7a
core2drew <calupesandro@gmail.com>
2024-05-13 19:32:12 +0800

    chore: Update to import App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { UpdateTitle } from "./topics/useEffect/WithDependency/UpdateTitle";
+import { MyFavoriteFruit } from "./topics/useState/ReadState/MyFavoriteFruit";
+import { Counter } from "./topics/useState/UpdateState/Counter";
+import { MyBio } from "./topics/useState/UpdateState/MyBio";
+import { UpdateMyFavoriteFruit } from "./topics/useState/UpdateState/UpdateMyFavoriteFruit";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* useState topics */}
+      {/* <MyFavoriteFruit /> */}
+      {/* <Counter /> */}
+      {/* <UpdateMyFavoriteFruit /> */}
+      {/* <MyBio /> */}
+
+      {/* useEffect topics */}
+      <UpdateTitle />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
